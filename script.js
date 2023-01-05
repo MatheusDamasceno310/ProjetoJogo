ad9b5afccc4df9c7269c0528a02f9256474366f5
MatheusDamasceno310 <matheus.damasceno310@gmail.com>
2023-01-05 18:42:52 -0300

    status Energia - OK

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 let c = parseFloat(localStorage.getItem("progressaoDoLevel"))
 let level =  parseInt(localStorage.getItem("levelUp"))
 let porcantagemLevel = parseFloat(localStorage.getItem("porcentagemLevel"))
+let contStatusEnergia = parseFloat(localStorage.getItem("contStatusEnergia"))
+let statusEnergiaRgb = parseFloat(localStorage.getItem("statusEnergiaRgb"))
+let statusEnergiarGb = parseFloat(localStorage.getItem("statusEnergiarGb"))
+let convPorcentagemEnergia = parseFloat(localStorage.getItem("convPorcentagemEnergia")) 
+
 
 if (localStorage.length > 0) {
 
@@ -12,14 +17,23 @@ if (localStorage.length > 0) {
     localStorage.setItem("levelUp", "1")
     localStorage.setItem("progressaoDoLevel" , "-100")
     localStorage.setItem("barraDeLevel", "translateX(-100%)")
-
+    localStorage.setItem("contStatusEnergia", "0")
+    localStorage.setItem("statusEnergiaRgb", "0")
+    localStorage.setItem("statusEnergiarGb", "255")
+    localStorage.setItem("statusDeEnergia", "linear-gradient(rgba(255, 255, 255, 0) 0%,rgb(0, 255, 0) 0%)")
+    localStorage.setItem("convPorcentagemEnergia", "100")
+    
 }
 
-document.getElementById('mostrarPorcentagemDeLevel').title = localStorage.getItem("porcentagemLevel");
+document.getElementById('mPorcentagemDeLevel').title = localStorage.getItem("porcentagemLevel");
+
+document.getElementById('mPorcentagemDaEnergia').title = "Energia: "+ localStorage.getItem("convPorcentagemEnergia") +"%"
 
 document.getElementById('level').innerText = localStorage.getItem("levelUp");
 
 document.getElementById('progressao').style.transform = localStorage.getItem("barraDeLevel");
+
+document.getElementById("energiaDoPet").style.background = localStorage.getItem("statusDeEnergia")
 
 function mudar() {
 
@@ -45,7 +59,7 @@ function mudar() {
 
             porcantagemLevel = porcantagemLevelSoma.toString() + "%"
 
-            document.getElementById('mostrarPorcentagemDeLevel').title = porcantagemLevel;
+            document.getElementById('mPorcentagemDeLevel').title = porcantagemLevel;
 
             localStorage.setItem("porcentagemLevel", porcantagemLevel)
             
@@ -74,7 +88,7 @@ function mudar() {
 
             porcantagemLevel = porcantagemLevelSoma.toString() + "%"
 
-            document.getElementById('mostrarPorcentagemDeLevel').title = porcantagemLevel;
+            document.getElementById('mPorcentagemDeLevel').title = porcantagemLevel;
 
             localStorage.setItem("porcentagemLevel", porcantagemLevel)
             
@@ -89,6 +103,57 @@ function mudar() {
             localStorage.setItem("barraDeLevel", progressao)
         }
 
-        
-    
 }
+
+setInterval(function() {
+    
+    if (contStatusEnergia < 100) {
+
+        statusEnergiaRgb += 5
+        localStorage.setItem("statusEnergiaRgb", statusEnergiaRgb.toString())
+
+        contStatusEnergia++
+
+        let porcantagemEnergiaSoma = contStatusEnergia - 100 
+
+        let convPorcentagemEnergia = porcantagemEnergiaSoma * -1
+
+        if (contStatusEnergia == 0) {
+            convPorcentagemEnergia = 100
+        }
+
+        localStorage.setItem("convPorcentagemEnergia", convPorcentagemEnergia.toString())
+
+        document.getElementById('mPorcentagemDaEnergia').title = "Energia: "+ localStorage.getItem("convPorcentagemEnergia") +"%"
+
+        localStorage.setItem("contStatusEnergia", contStatusEnergia.toString())
+
+        const StringTemp = contStatusEnergia + "%"
+
+        if (contStatusEnergia <= 65) {
+
+            const porcentagemDaEnergia = "linear-gradient(rgba(255, 255, 255, 0) "+ StringTemp +",rgb("+ statusEnergiaRgb.toString() +","+ statusEnergiarGb.toString() +", 0) 0%)"
+
+            localStorage.setItem("statusDeEnergia", porcentagemDaEnergia)
+
+        } else {
+            
+            statusEnergiarGb -= 15
+            localStorage.setItem("statusEnergiarGb", statusEnergiarGb.toString())
+
+            const porcentagemDaEnergia = "linear-gradient(rgba(255, 255, 255, 0) "+ StringTemp +",rgb("+ statusEnergiaRgb.toString() +","+ statusEnergiarGb.toString() +", 0) 0%)"
+
+            localStorage.setItem("statusDeEnergia", porcentagemDaEnergia)
+        }
+
+    }
+    
+    document.getElementById("energiaDoPet").style.background = localStorage.getItem("statusDeEnergia")
+
+} , 60000);
+
+
+
+
+    
+
