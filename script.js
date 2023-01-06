3a7d8de68091c5ba46714ac145955a03b5015ed8
MatheusDamasceno310 <matheus.damasceno310@gmail.com>
2023-01-05 22:12:20 -0300

    status Saude vinculado com a Energia

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ let statusEnergiaRgb = parseFloat(localStorage.getItem("statusEnergiaRgb"))
 let statusEnergiarGb = parseFloat(localStorage.getItem("statusEnergiarGb"))
 let convPorcentagemEnergia = parseFloat(localStorage.getItem("convPorcentagemEnergia")) 
 
+let contStatusSaude = parseFloat(localStorage.getItem("contStatusSaude"))
+let statusSaudeRgb = parseFloat(localStorage.getItem("statusSaudeRgb"))
+let statusSauderGb = parseFloat(localStorage.getItem("statusSauderGb"))
+let convPorcentagemSaude = parseFloat(localStorage.getItem("convPorcentagemSaude")) 
+
 
 if (localStorage.length > 0) {
 
@@ -22,6 +27,12 @@ if (localStorage.length > 0) {
     localStorage.setItem("statusEnergiarGb", "255")
     localStorage.setItem("statusDeEnergia", "linear-gradient(rgba(255, 255, 255, 0) 0%,rgb(0, 255, 0) 0%)")
     localStorage.setItem("convPorcentagemEnergia", "100")
+
+    localStorage.setItem("contStatusSaude", "0")
+    localStorage.setItem("statusSaudeRgb", "0")
+    localStorage.setItem("statusSauderGb", "255")
+    localStorage.setItem("statusDeSaude", "linear-gradient(rgba(255, 255, 255, 0) 0%,rgb(0, 255, 0) 0%)")
+    localStorage.setItem("convPorcentagemSaude", "100")
     
 }
 
@@ -29,14 +40,17 @@ document.getElementById('mPorcentagemDeLevel').title = localStorage.getItem("por
 
 document.getElementById('mPorcentagemDaEnergia').title = "Energia: "+ localStorage.getItem("convPorcentagemEnergia") +"%"
 
+document.getElementById('mPorcentagemDaSaude').title = "Saúde: "+ localStorage.getItem("convPorcentagemSaude") +"%"
+
 document.getElementById('level').innerText = localStorage.getItem("levelUp");
 
 document.getElementById('progressao').style.transform = localStorage.getItem("barraDeLevel");
 
 document.getElementById("energiaDoPet").style.background = localStorage.getItem("statusDeEnergia")
 
-function mudar() {
+document.getElementById("saudeDoPet").style.background = localStorage.getItem("statusDeSaude")
 
+function mudar() {
         if (c == 0) {
 
             if(level == 0) {
@@ -105,6 +119,8 @@ function mudar() {
 
 }
 
+/**Status Energia */
+
 setInterval(function() {
     
     if (contStatusEnergia < 100) {
@@ -152,8 +168,57 @@ setInterval(function() {
 
 } , 60000);
 
+/**Status de Energia Zerado */
 
+setInterval(function() {
+    
+    if (contStatusEnergia == 100) {
+        if (contStatusSaude < 100) {
 
+            statusSaudeRgb += 5
+            localStorage.setItem("statusSaudeRgb", statusSaudeRgb.toString())
+    
+            contStatusSaude++
+    
+            let porcantagemSaudeSoma = contStatusSaude - 100 
+    
+            let convPorcentagemSaude = porcantagemSaudeSoma * -1
+    
+            if (contStatusSaude == 0) {
+                convPorcentagemSaude = 100
+            }
+    
+            localStorage.setItem("convPorcentagemSaude", convPorcentagemSaude.toString())
+    
+            document.getElementById('mPorcentagemDaSaude').title = "Saúde: "+ localStorage.getItem("convPorcentagemSaude") +"%"
+    
+            localStorage.setItem("contStatusSaude", contStatusSaude.toString())
+    
+            const StringTemp = contStatusSaude + "%"
+    
+            if (contStatusSaude <= 65) {
+    
+                const porcentagemDaSaude = "linear-gradient(rgba(255, 255, 255, 0) "+ StringTemp +",rgb("+ statusSaudeRgb.toString() +","+ statusSauderGb.toString() +", 0) 0%)"
+    
+                localStorage.setItem("statusDeSaude", porcentagemDaSaude)
+    
+            } else {
+                
+                statusSauderGb -= 15
+                localStorage.setItem("statusSauderGb", statusSauderGb.toString())
+    
+                const porcentagemDaSaude = "linear-gradient(rgba(255, 255, 255, 0) "+ StringTemp +",rgb("+ statusSaudeRgb.toString() +","+ statusSauderGb.toString() +", 0) 0%)"
+    
+                localStorage.setItem("statusDeSaude", porcentagemDaSaude)
+            }
+    
+        }
+        
+        document.getElementById("saudeDoPet").style.background = localStorage.getItem("statusDeSaude")
+    }
 
+    
+
+} , 5000);
     
 
